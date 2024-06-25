5291c9f8b1bb3eab37120f7f2a42a91b3a49e64e
truongalice9190 <truongalice9190@gmail.com>
2024-06-25 02:26:28 -1100

    clock

---
+++ epro/src/components/App.js
@@ -0,0 +1,12 @@
+// App.js
+import React from 'react';
+import Clocks from './Clocks';
+const App = () => {
+  return (
+    <div>
+      <Clocks/>
+    </div>
+  );
+};
+
+export default App; // Đảm bảo đã export default App
